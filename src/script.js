4c1fd2eaa5ebc69ba0f34d017ac8f00b800457b5
Andika <andikaraditya1@gmail.com>
2023-12-29 11:01:54 +0700

    add more geometry and add dat.gui

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import * as dat from "dat.gui";
 
 const renderer = new THREE.WebGLRenderer()
 
@@ -9,26 +10,67 @@ document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-const orbit = new OrbitControls(camera, renderer.domElement);
+const orbit = new OrbitControls(camera, renderer.domElement)
 
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
+const gridHelper = new THREE.GridHelper(30)
+scene.add(gridHelper)
 
-camera.position.set(0, 2, 5)
+camera.position.set(-10, 30, 30)
 orbit.update()
 
 const boxGeometry = new THREE.BoxGeometry()
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const boxMaterial = new THREE.MeshBasicMaterial({ 
+    color: 0x00ff00, 
+    side: THREE.DoubleSide
+})
 const box = new THREE.Mesh(boxGeometry, boxMaterial)
 scene.add(box)
 
-function animate() {
-    box.rotation.x += 0.01
-    box.rotation.y += 0.01
-    renderer.render(scene, camera)
+const planeGeometry = new THREE.PlaneGeometry(30, 30)
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff })
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+scene.add(plane)
+plane.rotation.x = -0.5 * Math.PI
+
+const sphereGeometry = new THREE.SphereGeometry(5)
+const sphereMaterial = new THREE.MeshBasicMaterial({ 
+    color: 0x0000ff
+})
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+scene.add(sphere)
+sphere.position.set(10, 10,0)
+
+const gui = new dat.GUI();
+
+const options = {
+    sphereColor: "#ffea00",
+    wireframe: false,
+    speed: 0.01
 }
 
+gui.add(options, "sphereColor").onChange((e) => {
+    sphere.material.color.set(e)
+})
+gui.add(options, "wireframe").onChange((e) => {
+    sphere.material.wireframe = e
+})
+
+gui.add(options, "speed", 0.01)
+
+let step = 0
+
+function animate(time) {
+    box.rotation.x = time / 1000
+    box.rotation.y = time / 1000
+
+    step += options.speed
+    sphere.position.y = 10 * Math.abs(Math.sin(step))
+
+    renderer.render(scene, camera)
+}
 
 renderer.setAnimationLoop(animate)
